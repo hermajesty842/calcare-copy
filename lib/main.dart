29f8be457131250373c7a06cf0f202f80b1b4aa2
hermajesty842 <hildakinya17@gmail.com>
2026-03-06 18:21:24 +0300

    fix:added container widgets to my settings page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practice/widgets/widget-tree.dart';
+import 'package:practice/widgets/widget_tree.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/views/pages/tools.dart
+++ lib/views/pages/tools.dart
@@ -40,7 +40,7 @@ List<Map<String, dynamic>> gridItems = [
   },
 ];
 
-List<String> FAQS = [
+List<String> fAQS = [
   "How to check Repairs status?",
   "How to maka a reservation for in-store repairs?",
   "How to check the price of spare parts?",
@@ -52,97 +52,101 @@ class Tools extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "search",
-                prefixIcon:Icon(Icons.search_rounded,color: Colors.grey),
-                suffixIcon: Icon(Icons.qr_code_scanner_rounded,color: Colors.black,),
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "search",
+                    prefixIcon:Icon(Icons.search_rounded,color: Colors.grey),
+                    suffixIcon: Icon(Icons.qr_code_scanner_rounded,color: Colors.black,),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.transparent),
+                    )
+                  ),
+                ),
+              ),
+              Container(
+                // padding: EdgeInsets.all(30.0),
+                height: 200,
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 33, 194, 243),
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/quote.jpg"),
+                    fit: BoxFit.cover,
+                  ),
                   borderRadius: BorderRadius.circular(8.0),
-                  borderSide: BorderSide(color: Colors.transparent),
-                )
+                  shape: BoxShape.rectangle,
+                ),
               ),
-            ),
-          ),
-          Container(
-            // padding: EdgeInsets.all(30.0),
-            height: 150,
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 33, 194, 243),
-              image: DecorationImage(
-                image: AssetImage("assets/images/quote.jpg"),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(8.0),
-              shape: BoxShape.rectangle,
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            height: 180,
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 33, 194, 243),
-              borderRadius: BorderRadius.circular(8.0),
-              shape: BoxShape.rectangle,
-            ),
-            child: GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 10,
-              ),
-              itemCount: 8,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Colors.white,
-                      child: Icon(
-                        gridItems[index]['icon'],
-                        color: gridItems[index]['color'],
+              SizedBox(height: 10),
+              Container(
+                height: 230,
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 33, 194, 243),
+                  borderRadius: BorderRadius.circular(8.0),
+                  shape: BoxShape.rectangle,
+                ),
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 5,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemCount: 8,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            gridItems[index]['icon'],
+                            color: gridItems[index]['color'],
+                            ),
                         ),
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                    gridItems[index]['title'],
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 11),
-                    ),
+                        SizedBox(height: 5),
+                        Text(
+                        gridItems[index]['title'],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 11),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+          
+              Container(
+                height: 150,
+                width: double.infinity,
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ...fAQS.map((faqsString) {
+                      return Text(faqsString);
+                    }),
                   ],
-                );
-              },
-            ),
+                ),
+              ),
+            ],
           ),
-
-          Container(
-            height: 100,
-            width: double.infinity,
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.lightBlue,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ...FAQS.map((faqsString) {
-                  return Text(faqsString);
-                }).toList(),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
